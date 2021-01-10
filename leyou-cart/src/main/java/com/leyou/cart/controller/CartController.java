54cbc59b6b31b002036d5003e14b7e218b6eac69
林振坤 <65596854+1654350329@users.noreply.github.com>
2021-01-10 20:07:36 +0800

    修改购物车无法获取用户登入BUG

--- leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
+++ leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
@@ -21,7 +21,7 @@ public class CartController {
      *
      * @return
      */
-    @PostMapping
+    @PostMapping("addCart")
     public ResponseEntity<Void> addCart(@RequestBody Cart cart) {
         this.cartService.addCart(cart);
         return ResponseEntity.ok().build();
@@ -32,7 +32,7 @@ public class CartController {
      *
      * @return
      */
-    @GetMapping
+    @GetMapping("list")
     public ResponseEntity<List<Cart>> queryCartList() {
         List<Cart> carts = this.cartService.queryCartList();
         if (carts == null) {
